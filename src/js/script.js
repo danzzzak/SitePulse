ce0501ca3f39be2a597e72bb975562003c14287a
danzzzak <siriuz2000@mail.ru>
2020-10-17 05:45:06 +0500

    Карта и модальные окна

--- src/js/script.js
+++ src/js/script.js
@@ -39,3 +39,18 @@ function toggleSlide(item) {
 
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
+
+// MODAL
+
+$('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn();
+});
+$('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+$('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn();
+    });
+});
